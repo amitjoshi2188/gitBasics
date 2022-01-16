fdbc7e1c51fa98662dc7c4aea7487e75ed8e7de4
amitjoshi2188 <Amitjoshi2188@gmail.com>
2022-01-16 23:41:09 +0530

    Inital Commit

---
+++ greetings.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+    <title>Main file</title>
+
+    <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+    <script src="js/mainOne.js"></script>
+</head>
+
+<body>
+    <?php
+    echo 'Good Afternoon';
+    # code at line 3.
+
+    # code at line 5.
+
+    echo "Last line...................";
+    ?>
+</body>
+
+</html>
---
+++ js/mainOne.js
@@ -0,0 +1,12 @@
+$(function(){
+console.log("mainOne js called");
+});
+
+//let info = (str) => console.log(str);
+function infor (str){
+    console.log('Hello from infor...');    
+}
+
+//console.log(info('Hello...'));
+let item = infor('Hello...');
+console.log(item);
